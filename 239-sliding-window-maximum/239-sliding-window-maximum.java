276aeba03e1df5c5eb43e835ecdc659d8b76876e
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-15 19:47:02 +0100

    Time: 76 ms (23.08%), Space: 131.1 MB (31.12%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        Deque<Integer> deque = new LinkedList<Integer>();
+        
+        int start = 0;
+        int end = 0;
+        int len = nums.length;
+        int[] result = new int[len - k + 1];
+        int index = 0;
+        while(end < len){
+            
+            
+            while(!deque.isEmpty() && nums[end] >= nums[deque.peekLast()]){
+                deque.pollLast();
+            }
+            deque.addLast(end);
+            // if(!deque.isEmpty()){
+            //     if(nums[end] < nums[deque.peekLast()]){
+            //         while(!deque.isEmpty() && nums[end] > nums[deque.peekFirst()]){
+            //            deque.pollFirst(); 
+            //         }
+            //         deque.addFirst(end);
+            //     }
+            //     if(nums[end] >= nums[deque.peekLast()]){
+            //         deque.pollLast();
+            //         deque.addLast(end);
+            //     }
+            // }else{
+            //     deque.add(end);
+            // }
+            
+            if(end - start + 1 == k){
+                
+                result[index] = nums[deque.peekFirst()];
+                index++;
+                start++;
+                while(!deque.isEmpty() && deque.peek() < start){
+                    deque.poll();   
+                }
+                
+                
+            }
+            
+            end++;
+            
+            
+        }
+        return result;
+    }
+}
