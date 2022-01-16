88ae773a9f88e2277d2d7fbe3f445a17f9c75b6d
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-16 11:41:09 +0100

    Time: 77 ms (22.67%), Space: 131.3 MB (30.06%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         
+        //saving the index of the current maximum of the window in deque at the first position
         Deque<Integer> deque = new LinkedList<Integer>();
         
         int start = 0;
@@ -10,7 +11,17 @@ class Solution {
         int index = 0;
         while(end < len){
             
-            
+            /*whenever we encounter any maximum number than the last element in the deque
+            * remove the last element and insert the present element(maximum number) and continue
+            * till the present element becomes maximum than all the elements.
+            * 
+            * Why we do this ? --> We do this because if we encounter any maximum element then that element is               * going to be maximum for the previous windows.
+            * 
+            * But what if the present number is smaller than the last element in the deque...then we just simply 
+            * put it in the queue as there is a chance that present number(which is currently smaller) can be               * maximum for the upcoming windows.
+            *
+            * NOTE : Here we are storing the indices of the numbers in the array
+            */
             while(!deque.isEmpty() && nums[end] >= nums[deque.peekLast()]){
                 deque.pollLast();
             }
@@ -21,6 +32,8 @@ class Solution {
                 result[index] = nums[deque.peekFirst()];
                 index++;
                 start++;
+                
+                //Remove indices which are smaller than the starting index of the window
                 while(!deque.isEmpty() && deque.peek() < start){
                     deque.poll();   
                 }
