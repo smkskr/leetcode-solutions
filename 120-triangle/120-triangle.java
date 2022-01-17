895572d62b73a1934a82ec2aaf198e3882029a63
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-17 20:11:13 +0100

    Time: 10 ms (16.93%), Space: 41.1 MB (26.36%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        int len = triangle.size();
+        int res = Integer.MAX_VALUE;
+        
+        if(len == 1)return triangle.get(0).get(0);
+        
+        for(int index = 1;index < len;index++){
+            
+            List<Integer> currList = triangle.get(index);
+            
+            
+            List<Integer> prevList = triangle.get(index - 1);
+            
+            int size = currList.size();
+            int prevListSize = prevList.size();
+            
+            for(int i = 0;i < size;i++){
+                 
+              // if(i != 0){
+                    
+                    int prevListAboveElement = (i == prevListSize) ? Integer.MAX_VALUE : prevList.get(i);
+                    int prevListLeftDiagonalElement = (i - 1 == -1) ? Integer.MAX_VALUE : prevList.get(i - 1);
+                    
+                    
+                    currList.set(i, currList.get(i) + Math.min(prevListAboveElement,prevListLeftDiagonalElement));
+                
+                 if(index == len - 1)//calculate min value of last index
+                 res = Math.min(res, currList.get(i));
+                //}
+            }
+            
+           
+        }
+        
+         return res;
+    }
+}
