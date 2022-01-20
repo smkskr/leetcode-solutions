ec2f9637d00d6098b6fa887aa8db5d6fdac04725
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-20 12:16:37 +0100

    Time: 6 ms (12.81%), Space: 42.9 MB (31.55%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        for(int row = 0;row < rows;row++){
+            for(int col = 0;col < cols;col++){
+                
+                if(row == 0 && col == 0)continue;
+                
+                else{
+                    int up = row - 1 == -1 ? Integer.MAX_VALUE : grid[row - 1][col];
+                    int left = col - 1 == -1 ? Integer.MAX_VALUE : grid[row][col - 1];
+                    
+                    grid[row][col] = grid[row][col] + Math.min(up,left);
+                }
+            }
+        }
+        
+        return grid[rows - 1][cols - 1];
+    }
+}
