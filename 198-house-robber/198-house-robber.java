a98d98a7829dba580f06177f4cc55aefe81639c8
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-21 15:02:47 +0100

    Time: 0 ms (100.00%), Space: 38.4 MB (15.38%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        
+        return solve(nums, nums.length - 1, dp);
+    }
+    
+    public int solve(int[] nums, int n, int[] dp){
+        
+        if(n == 0)return dp[0] = nums[0];
+        
+        if(n == 1)return dp[1] = Math.max(nums[0], nums[1]);
+        
+        if(dp[n] != -1)return dp[n];
+        
+        dp[n] = Math.max(solve(nums, n - 1, dp), nums[n] + solve(nums, n - 2, dp));
+        
+        return dp[n];
+    }
+}
