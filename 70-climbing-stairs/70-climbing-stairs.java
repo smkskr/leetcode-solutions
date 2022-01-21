1b7ee4b61954d6843186c656147a50aa5dcdf630
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-21 13:38:21 +0100

    Time: 1 ms (6.77%), Space: 37.4 MB (40.20%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+       int[] dp = new int[n+1];
+       Arrays.fill(dp, -1);
+       return solve(n, dp);
+       
+    }
+    
+    //recursive solution
+    public int solve(int n, int[] dp){
+        
+        
+        if(n == 1 || n == 2 || n == 0)return n;
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        
+        return dp[n];
+    }
+}
