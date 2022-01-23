c0b16347c59d644ad61c638c2512cd19438a25f9
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-23 01:05:05 +0100

    Time: 1 ms (99.98%), Space: 36.2 MB (35.67%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         
-        if(n == Integer.MIN_VALUE) return false;
-        return n != 0 && (n & (n - 1)) == 0;
+        
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
