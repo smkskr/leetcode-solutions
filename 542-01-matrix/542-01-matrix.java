275c93a197235297c35e6beff196ca7ccc8a2002
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-23 22:23:14 +0100

    Time: 11 ms (80.42%), Space: 84.4 MB (39.43%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        
+       int[][] result = new int[mat.length][mat[0].length];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                result[i][j] = Integer.MAX_VALUE - 20000; //to avoid overflow
+            }
+        }
+        //first pass (checking left and top)
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
+                    result[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        result[i][j] = Math.min(result[i][j], result[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        result[i][j] = Math.min(result[i][j], result[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+        //second pass (checking right and bottom)
+        for (int i = mat.length - 1; i >= 0; i--) {
+            for (int j = mat[0].length - 1; j >= 0; j--) {
+                if (i < mat.length - 1) {
+                    result[i][j] = Math.min(result[i][j], result[i + 1][j] + 1);
+                }
+                if (j < mat[0].length - 1) {
+                    result[i][j] = Math.min(result[i][j], result[i][j + 1] + 1);
+                }
+            }
+        }
+        return result;
+    }
+}
