1bc0434b1219da7b870bbc95330f053ca04d8b61
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-23 12:52:38 +0100

    Time: 31 ms (17.98%), Space: 43.4 MB (24.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,13 +5,37 @@ class Solution {
         int length2 = text2.length();
         
         int[][] dp = new int[length1 + 1][length2 + 1];
+        
+        //bottom up approach
+        for(int row = 0;row <= length1;row++){
+            for(int col = 0;col <= length2;col++){
+                if(row == 0 || col == 0)dp[row][col] = 0;
+                else{
+                    if(text1.charAt(row - 1) == text2.charAt(col - 1)){
+                        dp[row][col] = 1 + dp[row - 1][col - 1];
+                    }else{
+                        dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
+                    }
+                }
+               
+            }
+        }
+        
+        return dp[length1][length2];
+        /**
+        *
+        * Following lines need to be uncommented when calling memoization solution
+        *
         for(int[] row : dp)
         Arrays.fill(row, -1);
         
         return LCS(text1, text2, text1.length(), text2.length(), dp);
+        **/
     }
     
-    //recursive solution using memoization
+    
+    
+    //recursive solution using memoization(top - down)
     public int LCS(String text1, String text2, int length1, int length2, int[][] dp){
         
         if(length1 == 0 || length2 == 0)return dp[length1][length2] = 0;
