b225053ff4c72606c64b7409f32b16a893b0c247
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-01-27 16:51:36 +0100

    Time: 1 ms (95.77%), Space: 50.3 MB (15.63%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -3,6 +3,12 @@ class Solution {
         
         int result = 0;
         
+        // The idea here is to take XOR of each and every element.
+        // But why XOR ? 
+        // XOR has the property of returning value zero whenever two bits are same, 
+        // hence whenever a number is XOR'd with it's duplicate number it returns as 0
+        // and therefor the only element which is not present twice will not be cancelled out with XOR
+        // and will give the result
         for(int num : nums){
             result ^= num; 
         }
