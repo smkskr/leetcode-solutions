c008d9d131a39163cfdabac9e0f193a32eeb36c8
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-01 19:34:38 +0100

    Time: 3 ms (90.06%), Space: 64.8 MB (5.12%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,40 +1,39 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
         
+        int totalSum = 0;
         
         int len = nums.length;
         
-        if(len == 0)return 0;
-        if(len == 1)return nums[0];
+        int normalSubArraySum = solveByKadane(nums, len);
         
-        int maxSubArraySum = solveByKadane(nums, len);
+        if(normalSubArraySum < 0)return normalSubArraySum;
         
-        if(maxSubArraySum < 0)return maxSubArraySum;
+        //find minimum sum of the array and subtract it from the totalSum
+        //to find minimum sum of the array we will again use Kadane by changing the sign of all the array elements
+        //this will give us the maximum sum with signs reversed(which is the minimum sum with original sign)
+        //now instead of subtracting it from totalSum we will add it since totalSum - (-minSum) = totalSum + minSum(maxSum with sign reversed)
         
-        int totalSum = 0;
-        
-        for(int i = 0;i < len;i++){
+        for(int i = 0; i < len;i++){
             totalSum += nums[i];
             nums[i] = - nums[i];
         }
         
-        int maxSubArrayCircularSum = solveByKadane(nums, len);
+        int minSubArraySumWithSignsReversed = solveByKadane(nums, len);
         
-        return Math.max(maxSubArraySum, totalSum + maxSubArrayCircularSum);
-        
+        return Math.max(normalSubArraySum, totalSum + minSubArraySumWithSignsReversed);
     }
     
-    public int solveByKadane(int[] nums,int len){
+    public int solveByKadane(int[] nums, int len){
+        
+        int maxSum = nums[0];
+        int sum = nums[0];
         
         
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
         
-        for(int i = 0;i < len;i++){
-            
+        for(int i = 1;i < len;i++){
             sum = Math.max(sum + nums[i], nums[i]);
             maxSum = Math.max(sum, maxSum);
-            
         }
         
         return maxSum;
