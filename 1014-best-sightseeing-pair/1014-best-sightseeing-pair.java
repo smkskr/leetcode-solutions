827cbe945d25a73529ce93b4299ed0a72a0d24b5
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-02 19:26:23 +0100

    Time: 9 ms (7.66%), Space: 63.8 MB (5.18%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -2,16 +2,17 @@ class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         
         int len = values.length;
-        int maxI = 0, max = 0;
-        for(int i = 0; i < len - 1;i++){
-            
-            maxI = Math.max(maxI, values[i] + i);
-            max = Math.max(max, maxI + values[i + 1] - (i + 1));
-            
+        int maxStart = 0;
+        int maxOverall = 0;
+        /*
+        * calculating max values[i]+i except the last element as the last element cannot be used as the starting point
+        * calculating maxOverall with sum of maxStart + values[i+1]-i+1 except the first element as the first element cannot be           * used as the ending point. we are doing i + 1 since ending point must be greater than starting point;
+        */
+        for(int i = 0;i < len - 1;i++){
+            maxStart = Math.max(maxStart, values[i] + i);
+            maxOverall = Math.max(maxOverall, maxStart + values[i+1] - (i + 1));
         }
         
-        return max;
-            
-            
+        return maxOverall;
     }
 }
