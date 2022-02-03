cdb1dfe2a87754e432fab5448d0e3d5762f3d9c6
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-03 12:36:33 +0100

    Time: 9 ms (71.03%), Space: 55.3 MB (17.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -4,10 +4,14 @@ class Solution {
         HashMap<String, List<String>> anagramMap = new HashMap<>();
         int len = strs.length;
         if(len == 0)return new ArrayList<>(anagramMap.values());
+        
         for(String str : strs){
+            
             char[] ch = str.toCharArray();
-            Arrays.sort(ch);
+            Arrays.sort(ch);// sorting each and every string by its characters and mapping those strings into one group
+            
             String key = String.valueOf(ch);
+            
             if(anagramMap.containsKey(key)){
                 List<String> currList = anagramMap.get(key);
                 currList.add(str);
@@ -17,6 +21,7 @@ class Solution {
                 currList.add(str);
                 anagramMap.put(key, currList);
             }
+            
         }
         
         return new ArrayList<>(anagramMap.values());
