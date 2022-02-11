9615ad82878d44e0bf635c80810e2b66f5347be2
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-11 19:05:16 +0100

    Time: 0 ms (100.00%), Space: 43.6 MB (11.40%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -19,7 +19,7 @@ class Solution {
         
         if(root == null)return false;
         
-        if(targetSum - root.val == 0 && root.left == null && root.right == null){
+        if(root.left == null && root.right == null && targetSum - root.val == 0){
             return true;
         }
     
