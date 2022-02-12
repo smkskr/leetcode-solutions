ecf6f8189a912f4124891ab60478ce707c1e39f7
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-12 03:51:05 +0100

    Time: 0 ms (100.00%), Space: 52.2 MB (5.51%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -16,13 +16,20 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         
-        if(root == null)return root;
+        return recursiveSolution(root, val);
+    }
+    
+    //recursive solution
+    public TreeNode recursiveSolution(TreeNode root, int val){
+         if(root == null)return root;
         
         if(root.val == val)return root;
         
         if(val < root.val){
-            return searchBST(root.left, val);    
+            return recursiveSolution(root.left, val);    
         }
-        return searchBST(root.right, val);
+        return recursiveSolution(root.right, val);
     }
+    
+    //iterative solution
 }
