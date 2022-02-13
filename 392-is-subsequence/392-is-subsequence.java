41aaaa1d7cdeb1821662a8da80a7fdaa17e6ff71
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-13 14:35:05 +0100

    Time: 0 ms (100.00%), Space: 42.3 MB (7.85%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
-        int leftPointer = 0;
+        int lenS = s.length();
+        int lenT = t.length();
         
-        int tLength = t.length();
-        int sLength = s.length();
-        
-        if(sLength == 0)return true;
-        
-        if(tLength == 0){
-            return sLength == 0;
-        }
-        
-        for(int index = 0;index < tLength;index++){
-            if(s.charAt(leftPointer) == t.charAt(index)){
-                leftPointer++;
+        if(lenS == 0)return true;
+        if(lenT == 0)return lenS == 0;
+        if(lenS > lenT)return false;
+        int indexS = 0;
+        for(int index = 0;index < lenT;index++){
+            char charT = t.charAt(index);
+            if(charT == s.charAt(indexS)){
+                indexS++;
+                if(indexS == lenS)return true;
             }
-            if(leftPointer == sLength)return true;
         }
         
         return false;
