7deb3d696dccc2cb030d8590dfc961613b31be97
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-13 14:13:50 +0100

    Time: 5 ms (24.04%), Space: 46 MB (14.87%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -7,13 +7,14 @@ class Solution {
         for(int row = 0;row < rows;row++){
             for(int col = 0;col < cols;col++){
                 
-                if(row == 0 && col == 0)continue;
-                
+                if(row == 0 & col == 0)continue;
                 else{
+                    
                     int up = row - 1 == -1 ? Integer.MAX_VALUE : grid[row - 1][col];
                     int left = col - 1 == -1 ? Integer.MAX_VALUE : grid[row][col - 1];
                     
-                    grid[row][col] = grid[row][col] + Math.min(up,left);
+                    grid[row][col] += Math.min(up, left);
+                    
                 }
             }
         }
@@ -21,3 +22,5 @@ class Solution {
         return grid[rows - 1][cols - 1];
     }
 }
+
+
