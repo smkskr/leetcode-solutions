2cd866f8620b7da7f5d12d4cdda9d32a3e88e0e3
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-13 18:21:45 +0100

    Time: 9 ms (92.16%), Space: 46 MB (27.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,53 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int length1 = text1.length();
-        int length2 = text2.length();
+        int len1 = text1.length();
+        int len2 = text2.length();
         
-        int[][] dp = new int[length1 + 1][length2 + 1];
+        int[][] dp = new int[len1 + 1][len2 + 1];
         
-        //bottom up approach
-        for(int row = 0;row <= length1;row++){
-            for(int col = 0;col <= length2;col++){
-                if(row == 0 || col == 0)dp[row][col] = 0;
-                else{
-                    if(text1.charAt(row - 1) == text2.charAt(col - 1)){
-                        dp[row][col] = 1 + dp[row - 1][col - 1];
-                    }else{
-                        dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
-                    }
+        for(int row = 1;row <= len1;row++){
+            for(int col = 1;col <= len2;col++){
+                
+                if(text1.charAt(row - 1) == text2.charAt(col - 1)){
+                    dp[row][col] = 1 + dp[row - 1][col - 1];
+                }else{
+                    dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
                 }
-               
             }
         }
         
-        return dp[length1][length2];
-        /**
-        *
-        * Following lines need to be uncommented when calling memoization solution
-        *
-        for(int[] row : dp)
-        Arrays.fill(row, -1);
+        return dp[len1][len2];
         
-        return LCS(text1, text2, text1.length(), text2.length(), dp);
-        **/
-    }
-    
-    
-    
-    //recursive solution using memoization(top - down)
-    public int LCS(String text1, String text2, int length1, int length2, int[][] dp){
-        
-        if(length1 == 0 || length2 == 0)return dp[length1][length2] = 0;
-        
-        if(dp[length1][length2] != -1)return dp[length1][length2];
-        
-        if(text1.charAt(length1 - 1) == text2.charAt(length2 - 1)){
-            dp[length1][length2] = 1 + LCS(text1, text2, length1 - 1, length2 -1, dp);
-        }else{
-            dp[length1][length2] = Math.max(LCS(text1, text2, length1, length2 - 1, dp), LCS(text1, text2, length1 - 1, length2, dp));
-        }
-        
-        return dp[length1][length2];
     }
 }
