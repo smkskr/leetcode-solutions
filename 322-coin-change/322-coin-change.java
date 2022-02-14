7fa798179dd1a5ec905441cb4380aa0b01ca8f1a
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-15 00:48:03 +0100

    Time: 23 ms (67.15%), Space: 45.3 MB (23.75%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,48 +1,47 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         
-        int[] dp = new int[amount + 1]; //array for storing minimum number of coins for a particular amount
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        
         dp[0] = 0;
         
-        for(int amt = 1;amt < dp.length;amt++){
+        for(int i = 1;i <= amount;i++){
             for(int coin = 0;coin < coins.length;coin++){
-                
-                if(coins[coin] <= amt){
-                    if(dp[amt - coins[coin]] != Integer.MAX_VALUE)
-                    dp[amt] = Math.min(dp[amt],1 + dp[amt - coins[coin]]);
+                if(coins[coin] <= i){
+                    if(dp[i - coins[coin]] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[coin]]);
                 }
             }
         }
         
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
         
-        /**
-        * recursive call
-        int res = solve(coins, amount);
-        return res == Integer.MAX_VALUE ? -1 : res;
-        */
+        //return recursiveCode(coins, amount);
+       
     }
     
-    //recursive solution
-    public int solve(int[] coins, int amount){
+    public int recursiveCode(int[] coins, int amount){
         
         if(amount == 0)return 0;
         
         int res = Integer.MAX_VALUE;
-        
-        for(int i = 0;i < coins.length;i++){
+        for(int coin = 0;coin < coins.length;coin++){
             
-            int sub_res;
-            if(coins[i] <= amount){
-                sub_res = solve(coins, amount - coins[i]);
-                if(sub_res != Integer.MAX_VALUE)
-                res = Math.min(res, 1 + sub_res);
+            int subRes;
+            if(coins[coin] <= amount){
+                subRes  = recursiveCode(coins, amount - coins[coin]);
+                if(subRes != Integer.MAX_VALUE){
+                    res = Math.min(res, 1 + subRes);
+                }
+              
             }
+            
+             
+           
+            
+            
         }
         
         return res;
-        
     }
 }
