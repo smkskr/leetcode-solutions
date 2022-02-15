e36fc1245780c4fa80330038c375c475a206b97c
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-15 02:57:54 +0100

    Time: 61 ms (5.92%), Space: 40.8 MB (19.94%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -1,22 +1,38 @@
 class Solution {
-    
-    int product = 1;
-    
     public int integerBreak(int n) {
-        return solve(n - 1 ,n);
+        
+//         int[] dp = new int[n + 1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         dp[2] = 1;
+        
+//         for(int value = 3;value <= n;value++){
+//             int subRes = 1;
+//             int target = value;
+//             for(int i = 1;i < n;i++){
+//                 if(i <= value){
+//                     subRes = i * dp[target - i];
+//                     target = target - i;
+//                 }
+                
+//             }
+//             dp[value] = Math.max(dp[value], subRes);
+//         }
+        
+      // return dp[n];
+       return solve(n - 1, n);
     }
     
-    public int solve(int start, int n){
+    public int solve(int n, int target){
         
-        if(start == 0)return 0;
-        if(n == 0)return 1;
+        if(target == 0)return 1;
+        if(n == 0)return 0;
+        
        
-        if(start <= n){
-            
-          return Math.max(start * solve(start, n - start),solve(start - 1, n));
+        if(n <= target){
+            return Math.max(n * solve(n, target - n),solve(n - 1,target));
         }else{
-            return  solve(start - 1, n);
+            return solve(n - 1, target);
         }
-        
     }
 }
