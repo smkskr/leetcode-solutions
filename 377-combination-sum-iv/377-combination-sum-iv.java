e889a670fbdc8833666b32d72c5beb9062350664
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-15 01:41:10 +0100

    Time: 1 ms (90.22%), Space: 41.7 MB (11.19%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,35 +1,32 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         
-        int len = nums.length;
-        
         int[] dp = new int[target + 1];
         dp[0] = 1;
         
-        for(int i = 1;i <= target;i++){
-            for(int j = 0;j < len;j++){
-                
-                if(nums[j] <= i)
-                dp[i] = dp[i] + dp[i - nums[j]];
+        for(int value = 1; value <= target;value++){
+            for(int num = 0;num < nums.length;num++){
+                if(nums[num] <= value){
+                    dp[value] += dp[value - nums[num]];
+                }
             }
         }
         
         return dp[target];
-        //return solve(nums, target);
+        //return recursiveCode(nums, target);
     }
     
-    //recursive solution
-    public int solve(int[] nums, int target){
+    public int recursiveCode(int[] nums, int target){
         
-        if(target < 0)return 0;
         if(target == 0)return 1;
+        if(target < 0)return 0;
         
         int res = 0;
         
         for(int i = 0;i < nums.length;i++){
             
             if(nums[i] <= target){
-                res += solve(nums,target - nums[i]);
+                res = res + recursiveCode(nums, target - nums[i]);
             }
         }
         
