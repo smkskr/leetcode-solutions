3d69c762bdff2100218f6a7b1c0f0efe222845eb
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-17 01:26:15 +0100

    Time: 0 ms (100.00%), Space: 42.9 MB (71.18%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+        int xor = 0;
+        for(int i = 1;i <= n;i++){
+            xor = xor ^ i;
+        }
+        
+        for(int i = 0;i < n;i++){
+            xor = xor ^ nums[i];
+        }
+        
+        return xor;
+    }
+}
