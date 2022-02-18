85392600a84dda7d724459689467a9258115d576
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-18 11:42:20 +0100

    Time: 28 ms (76.94%), Space: 62.5 MB (11.53%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,26 +1,23 @@
-//This is a sliding window problem(since it contains operations related to subarray) with variable size window
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
-     HashMap<Integer, Integer> freqMap = new HashMap<>();
-     int len = nums.length;
-     int sum = 0;
-     int count = 0;
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int sum = 0;
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0;i < n;i++){
+            
+            sum += nums[i];
+            if(sum == k){
+                count++;
+            }
+            
+            if(freqMap.containsKey(sum - k)){
+                count += freqMap.get(sum - k);
+            }
+            freqMap.put(sum,freqMap.getOrDefault(sum, 0)+ 1);
+        }
         
-     for(int index = 0;index < len;index++){
-        
-         sum = sum + nums[index];
-         if(sum == k){
-             count++;
-         }
-         if(freqMap.containsKey(sum - k)){
-            count = count + freqMap.get(sum - k);
-         }
-         freqMap.put(sum, freqMap.getOrDefault(sum, 0) + 1);
-         
-     }
-        
-     return count;
-       
+        return count;
     }
 }
