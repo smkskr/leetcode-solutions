d0c38b575344a9c8119a44b0f0d44863c517dbbe
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-02-23 21:45:07 +0100

    Time: 22 ms (8.74%), Space: 54 MB (5.00%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,31 +1,10 @@
+//This problem is a slight variation of coin change/Unbounded Knapsack
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
         List<List<Integer>> result = new ArrayList<>();
         int len = candidates.length;
-        
-        //array storing number of ways sum is possible..rows -> contain number of elements, cols -> contains the target sum
-        //int[][] dp = new int[len + 1][target + 1];
-        
-       
-        //if target == 0, then 1 solution is possible
-        //therefore, all the elements in the first col is going to be 1
-//         for(int row = 0;row <= len;row++){
-//             dp[row][0] = 1;
-//         }
-        
-//         for(int row = 1;row <= len;row++){
-//             for(int col = 1;col <= target;col++){
-                
-//                 if(candidates[row - 1] <= col){
-//                     dp[row][col] = dp[row][col - candidates[row - 1]] + dp[row - 1][col];
-//                 }else{
-//                     dp[row][col] = dp[row - 1][col];
-//                 }
-//             }
-//         }
-        
-        //result.add(new ArrayList<>(Arrays.asList(dp[len][target])));
+      
         solve(candidates, target, len - 1,result, new ArrayList<>());
         return result;
     }
@@ -47,7 +26,6 @@ class Solution {
             solve(candidates, target - candidates[index], index, result, newTemp);
             solve(candidates, target, index - 1,result,new ArrayList<>(temp));
         }else{
-             //temp.remove(temp.size() - 1);
              solve(candidates, target, index - 1,result, new ArrayList<>(temp));
         }
     }
