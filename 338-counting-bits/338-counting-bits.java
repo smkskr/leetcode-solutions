bd95f61dbeb8e536642905f5cbd6f9cc69a4da3d
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-06 14:38:45 +0100

    Time: 3 ms (50.16%), Space: 47.7 MB (66.23%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,13 +2,32 @@ class Solution {
     public int[] countBits(int n) {
         
         int[] dp = new int[n+1];
+        dp[0] = 0;
+        
+        for(int i = 1; i<= n;i++){
+            int quotient = i / 2;
+            if(i%2 == 0){
+                dp[i] = dp[quotient];
+            }else{
+                dp[i] = dp[quotient] + 1;
+            }
+        }
+        
+            return dp;
        
+    }
+    
+    //small three line code
+    public int[] countBitsShortCode(int n){
+        
+        int[] dp = new int[n + 1];
         
         for(int i = 0;i <=n;i++){
           dp[i] = dp[i/2] + i%2;
         }
         
         return dp;
+        
     }
     
    
