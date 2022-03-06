19a410f3860ed487529b57bd8f2605fd1e73a802
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-06 12:35:58 +0100

    Time: 0 ms (100.00%), Space: 43.1 MB (83.91%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         
-        int rows = image.length;
-        int cols = image[0].length;
-        
-        performDfs(image, sr, sc, newColor, image[sr][sc]);
- 
+        dfs(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
     
-    public void performDfs(int[][] image, int row, int col,int newColor, int sameColor){
+    public void dfs(int[][] image, int row, int col, int sameColor, int newColor){
         
-        if(row < 0 || row == image.length || col < 0 || col == image[0].length || image[row][col] != sameColor ){
+        if(row == -1 || row == image.length || col == -1 || col == image[0].length || image[row][col] != sameColor){
             return;
         }
         
-        if(image[row][col] == newColor)return;
-       
+        if(image[row][col] == newColor)return; 
         image[row][col] = newColor;
         
-        performDfs(image, row - 1, col, newColor, sameColor);//go up
-        performDfs(image, row + 1, col, newColor, sameColor);//go down
-        performDfs(image, row, col - 1, newColor, sameColor);//go left
-        performDfs(image, row, col + 1, newColor, sameColor);//go right
-        
+        dfs(image, row - 1,col, sameColor, newColor);//up
+        dfs(image, row + 1, col, sameColor, newColor);//down
+        dfs(image, row, col - 1, sameColor, newColor);//left
+        dfs(image, row, col + 1, sameColor, newColor);//right
     }
 }
