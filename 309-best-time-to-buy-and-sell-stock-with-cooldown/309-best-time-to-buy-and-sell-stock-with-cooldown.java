8553dbd5373c4ad2a509b819221b25e4568a1abd
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-31 01:31:32 +0200

    Time: 2 ms (44.28%), Space: 42.4 MB (39.03%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,52 +2,77 @@ class Solution {
     public int maxProfit(int[] prices) {
         
         int n = prices.length;
+        //return usingSpaceOptimizedDP(prices, n);
         return usingDP(prices, n);
-        //return solve(prices, 0, 1, 0);
+        //return solve(prices, 0, 1);
     }
+    
+    //using space optimized dp
+//     public int usingSpaceOptimizedDP(int[] prices, int n){
+        
+//         int[][] prevProfit = new int[2];
+//         int[][] currProfit = new int[2];
+        
+//         for(int index = n-1;index >= 0;index--){
+//             for(int buy = 0;buy < 2;buy++){
+               
+                        
+                   
+//                         if(buy == 1){
+//                            currProfit[buy][cooldown] = Math.max(-prices[index] + prevProfit[0][0], prevProfit[1][0]);    
+//                         }else{
+//                            currProfit[buy][cooldown] = Math.max(prices[index] + prevProfit[1][1], prevProfit[0][0]);
+//                         }
+                  
+                    
+                
+//                 prevProfit = currProfit;
+//             }
+            
+            
+//         }
+        
+//         return currProfit[1][0];
+//     }
     
     //using dp
     public int usingDP(int[] prices, int n){
           
-        int[][][] dp = new int[n+1][2][2];
+        int[][] dp = new int[n+2][2];
         
         for(int index = n-1;index >= 0;index--){
             for(int buy = 0;buy < 2;buy++){
-                for(int cooldown = 0;cooldown < 2;cooldown++){
+               
                         
-                    if(cooldown == 0){
-                        if(buy == 1){
-                            dp[index][buy][cooldown] = Math.max(-prices[index] + dp[index + 1][0][0], dp[index + 1][1][0]);    
-                        }else{
-                            dp[index][buy][cooldown] = Math.max(prices[index] + dp[index + 1][1][1], dp[index + 1][0][0]);
-                        }
-                    }else{
-                        dp[index][buy][cooldown] = dp[index + 1][1][0];
-                    }
                     
-                }
+                        if(buy == 1){
+                            dp[index][buy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);    
+                        }else{
+                            dp[index][buy] = Math.max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
+                        }
+                   
+                    
+                
             }
         }
         
-        return dp[0][1][0];
+        return dp[0][1];
     }
     
     //using recursion
-    public int solve(int[] prices, int index, int buy,int cooldown){
+    public int solve(int[] prices, int index, int buy){
         
-        if(index == prices.length)return 0;
+        if(index >= prices.length)return 0;
         
         int profit = 0;
         
-        if(cooldown == 0){
+       
             if(buy == 1){
-                profit = Math.max(-prices[index] + solve(prices, index + 1, 0, 0), 0 + solve(prices, index + 1, 1, 0));    
+                profit = Math.max(-prices[index] + solve(prices, index + 1, 0), 0 + solve(prices, index + 1, 1));    
             }else{
-                profit = Math.max(prices[index] + solve(prices, index + 1, 1, 1), 0 + solve(prices, index + 1, 0, 0));
+                profit = Math.max(prices[index] + solve(prices, index + 2, 1), 0 + solve(prices, index + 1, 0));
             }
-        }else{
-            profit = solve(prices, index + 1, 1, 0);
-        }
+        
         
         return profit;
         
