082578dab461355bf26599cc4962b90bb52ce7b4
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-31 01:36:28 +0200

    Time: 2 ms (44.28%), Space: 42.8 MB (18.11%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,38 +2,9 @@ class Solution {
     public int maxProfit(int[] prices) {
         
         int n = prices.length;
-        //return usingSpaceOptimizedDP(prices, n);
         return usingDP(prices, n);
         //return solve(prices, 0, 1);
     }
-    
-    //using space optimized dp
-//     public int usingSpaceOptimizedDP(int[] prices, int n){
-        
-//         int[][] prevProfit = new int[2];
-//         int[][] currProfit = new int[2];
-        
-//         for(int index = n-1;index >= 0;index--){
-//             for(int buy = 0;buy < 2;buy++){
-               
-                        
-                   
-//                         if(buy == 1){
-//                            currProfit[buy][cooldown] = Math.max(-prices[index] + prevProfit[0][0], prevProfit[1][0]);    
-//                         }else{
-//                            currProfit[buy][cooldown] = Math.max(prices[index] + prevProfit[1][1], prevProfit[0][0]);
-//                         }
-                  
-                    
-                
-//                 prevProfit = currProfit;
-//             }
-            
-            
-//         }
-        
-//         return currProfit[1][0];
-//     }
     
     //using dp
     public int usingDP(int[] prices, int n){
@@ -42,17 +13,12 @@ class Solution {
         
         for(int index = n-1;index >= 0;index--){
             for(int buy = 0;buy < 2;buy++){
-               
-                        
-                    
-                        if(buy == 1){
-                            dp[index][buy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);    
-                        }else{
-                            dp[index][buy] = Math.max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
-                        }
-                   
-                    
                 
+                if(buy == 1){
+                    dp[index][buy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);    
+                }else{
+                    dp[index][buy] = Math.max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
+                }    
             }
         }
         
