f30f37aecac3210a06a032ed58638afc7b143dcf
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-31 19:49:50 +0200

    Time: 35 ms (17.81%), Space: 81.3 MB (12.24%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,17 +1,41 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         
-        int len = prices.length;
+        int n = prices.length;
+        return usingDP(prices, n, fee);
         
-        int buy = - prices[0];
-        int sell = 0;
+       // return solve(prices, 0, 1, fee);
+    }
+    
+    
+    //using dp
+    
+    public int usingDP(int[] prices, int n, int fee){
         
-        for(int i = 1;i < len;i++){
+        int[][] dp = new int[n + 1][2];
+        
+        for(int index = n - 1;index >= 0;index--){
+           
+                dp[index][1] = Math.max(-prices[index] + dp[index + 1][0],  dp[index + 1][1]); 
+           
+                dp[index][0] = Math.max(prices[index] - fee + dp[index + 1][1],  dp[index + 1][0]);
             
-            sell = Math.max(sell, buy + prices[i] - fee);
-            buy = Math.max(buy, sell - prices[i]);
         }
         
-        return sell;
+        return dp[0][1];
+    }
+    
+    //using recursion
+    public int solve(int[] prices, int index, int buy,int fee){
+        
+        if(index == prices.length)return 0;
+        int profit = 0;
+        if(buy == 1){
+            profit = Math.max(-prices[index] + solve(prices, index + 1, 0, fee), solve(prices, index + 1, 1, fee)); 
+        }else{
+            profit = Math.max(prices[index] - fee + solve(prices, index + 1, 1, fee), solve(prices, index + 1, 0, fee));
+        }
+        
+        return profit;
     }
 }
