0aa97dcbac4f1b587dd1a4f0da6ec273a0677d84
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-03-31 19:52:47 +0200

    Time: 5 ms (65.16%), Space: 69.3 MB (66.40%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -2,14 +2,29 @@ class Solution {
     public int maxProfit(int[] prices, int fee) {
         
         int n = prices.length;
-        return usingDP(prices, n, fee);
-        
+        return usingSpaceOptimizedDP(prices, n, fee);
+        //return usingDP(prices, n, fee);
        // return solve(prices, 0, 1, fee);
     }
     
     
-    //using dp
+    //using space optimized DP
+     public int usingSpaceOptimizedDP(int[] prices, int n, int fee){
+        
+        int[] prevProfit = new int[2];
+        int[] currProfit = new int[2];
+        
+        for(int index = n - 1;index >= 0;index--){
+           
+                currProfit[1] = Math.max(-prices[index] + prevProfit[0],  prevProfit[1]); 
+                currProfit[0] = Math.max(prices[index] - fee + prevProfit[1],  prevProfit[0]);
+                prevProfit = currProfit;
+        }
+        
+        return currProfit[1];
+    }
     
+    //using dp
     public int usingDP(int[] prices, int n, int fee){
         
         int[][] dp = new int[n + 1][2];
