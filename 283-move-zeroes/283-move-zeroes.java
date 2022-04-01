1a7d8f15b05fce684ee5599f1d2a68d18ef4e06e
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-01 18:23:27 +0200

    Time: 2 ms (71.74%), Space: 54.8 MB (53.60%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -5,6 +5,7 @@ class Solution {
         int zeroPointer = 0;
         for(int i = 0;i < n;i++){
             
+            //if the number is not zero then swap it with zero value in the array
             if(nums[i] != 0){
                 int temp = nums[i];
                 nums[i] = nums[zeroPointer];
@@ -13,5 +14,27 @@ class Solution {
             }
         }
         
+    }
+    
+    //another approach
+    public void moveZeroes2(int[] nums) {
+        
+        int len = nums.length;
+        
+        int index = 0;
+        
+        for(int i = 0;i < len;i++){
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        
+        while(index < len){
+            nums[index] = 0;
+            index++;
+        }
+            
+            
     }
 }
