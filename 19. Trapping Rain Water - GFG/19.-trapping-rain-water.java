1b30c2c0af8202443dfd000839db8ae346383848
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-15 12:57:25 +0200

    Added solution - LeetHub

---
+++ 19. Trapping Rain Water - GFG/19.-trapping-rain-water.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        
+        // Your code here
+        
+        //find left highest element of an element
+        int[] leftHighest = new int[n];
+        leftHighest[0] = arr[0];
+        for(int i = 1;i < n;i++){
+            if(leftHighest[i - 1] < arr[i]){
+                leftHighest[i] = arr[i];
+            }else{
+                leftHighest[i] = leftHighest[i - 1];
+            }
+        }
+        
+        //find right highest element of an element
+        int[] rightHighest = new int[n];
+        rightHighest[n - 1] = arr[n - 1];
+         for(int i = n - 2;i >= 0;i--){
+            if(arr[i] > rightHighest[i + 1]){
+                rightHighest[i] = arr[i];
+            }else{
+                rightHighest[i] = rightHighest[i + 1];
+            }
+        }
+        
+        long sum = 0;
+        for(int i = 0;i < n;i++){
+            sum += Math.abs(arr[i] - Math.min(leftHighest[i],rightHighest[i]));
+        }
+        
+        return sum;
+        
+    }
+    
+    
+}
+
+
