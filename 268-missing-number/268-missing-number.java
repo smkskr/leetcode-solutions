ceefc0d103c8e25089339f10edc5aea4a734ca42
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-21 15:55:36 +0200

    Time: 1 ms (67.57%), Space: 43 MB (85.43%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -2,15 +2,31 @@ class Solution {
     public int missingNumber(int[] nums) {
         
         int n = nums.length;
-        int xor = 0;
-        for(int i = 1;i <= n;i++){
-            xor = xor ^ i;
+        
+        int start = 0;
+        
+        while(start < n){
+            
+            int correctIndex = nums[start];
+            if(nums[start] < n && nums[start] != nums[correctIndex]){
+                swap(nums, start, correctIndex);
+            }else{
+                start++;
+            }
         }
         
-        for(int i = 0;i < n;i++){
-            xor = xor ^ nums[i];
+        //CASE 1 : when missing number is within bounds of array
+        for(int index = 0;index < n;index++){
+            if(nums[index] != index)return index;
         }
         
-        return xor;
+        //CASE 2 : when missing number is outside of bounds
+        return n;
+    }
+    
+    public void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
