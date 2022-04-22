76da8bef2d3a791065971a86c93e3bd00b383ddb
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-22 10:51:08 +0200

    Time: 9 ms (51.21%), Space: 65.9 MB (61.49%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -23,11 +23,31 @@ class Solution {
         }
         
         return result;
+        
+        //return findDisappearedNumbersOneTimeParsing(nums);
     }
     
     public void swap(int[] nums, int first, int second){
         int temp = nums[first];
         nums[first] = nums[second];
         nums[second] = temp;
+    }
+    
+    public List<Integer> findDisappearedNumbersOneTimeParsing(int[] nums) {
+        
+        //mark the numbers as negative such that nums[index] = -(value at that index)
+        for(int i = 0;i < nums.length;i++){
+            int val = Math.abs(nums[i]) - 1;
+            if(nums[val] > 0){
+                nums[val] = nums[val]*(-1);
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i] > 0)
+            result.add(i+1);
+        }
+        
+        return result;
     }
 }
