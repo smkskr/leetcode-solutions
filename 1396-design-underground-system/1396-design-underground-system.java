5097a0409efc418d30757db55fb6bcc46049507d
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-24 18:38:26 +0200

    Time: 156 ms (56.79%), Space: 104.2 MB (28.19%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,75 @@
+class UndergroundSystem {
+    
+    Map<Integer, TripDetails> checkInMap;
+    //Map<Integer, TripDetails> checkOutMap;
+    Map<String, TripCount> tripTimeMap;
+    
+    public UndergroundSystem() {
+        
+        checkInMap = new HashMap<>();
+        //checkOutMap = new HashMap<>();
+        tripTimeMap = new HashMap<>();
+        
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new TripDetails(stationName,t));
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+       // checkOutMap.put(id, new TripDetails(stationName,t));
+        
+        TripDetails sourceTrip = checkInMap.get(id);
+        String source = sourceTrip.stationName;
+        int time = sourceTrip.time;
+        
+        String key = source + "_" + stationName;
+        
+        if(tripTimeMap.containsKey(key)){
+            TripCount obj = tripTimeMap.get(key);
+            obj.totalTime += (t - time);
+            obj.count++;
+            tripTimeMap.put(key, obj);
+        }
+        else
+        tripTimeMap.put(key, new TripCount(t - time, 1));
+
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        
+        String key = startStation + "_" + endStation;
+        TripCount obj = tripTimeMap.get(key);
+        return (double)obj.totalTime/obj.count;
+    }
+}
+
+class TripDetails{
+    
+    String stationName;
+    int time;
+    
+    TripDetails(String _stationName, int _time){
+        stationName = _stationName;
+        time = _time;
+    }
+}
+
+class TripCount{
+    int totalTime;
+    int count;
+    
+    TripCount(int _totalTime, int _count){
+        totalTime = _totalTime;
+        count = _count;
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
+
