2a81bd15716478feae24f1e3fb700d811683449b
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-30 21:26:11 +0200

    Time: 0 ms (100.00%), Space: 42.1 MB (55.89%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -2,19 +2,17 @@ class Solution {
     public int lengthOfLastWord(String s) {
         
         int len = s.length();
-        String lastWord = "";
+        int count = 0;
         
         for(int i = len - 1;i >= 0;i--){
             char ch = s.charAt(i);
             if(ch == ' '){
-                if(lastWord.length() != 0){
-                    break;
-                }
+               if(count > 0)break;
             }else{
-                lastWord += ch;
+               count++;
             }
         }
         
-        return lastWord.length();
+        return count;
     }
 }
