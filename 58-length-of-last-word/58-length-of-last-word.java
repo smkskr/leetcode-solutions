2acb29f2f9844897be281a9ceb0b8bc5ad49390d
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-04-30 21:31:26 +0200

    Time: 0 ms (100.00%), Space: 41.8 MB (68.48%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -2,17 +2,21 @@ class Solution {
     public int lengthOfLastWord(String s) {
         
         int len = s.length();
-        int count = 0;
+        int countLetter = 0;
         
+        //Since we are looking for the last word ; run a reverse loop
         for(int i = len - 1;i >= 0;i--){
             char ch = s.charAt(i);
+            
+            //if the character is a space then check whether there is any letter present or not, if present then break the loop
+            //if the character is not a space then increment the letter count
             if(ch == ' '){
-               if(count > 0)break;
+               if(countLetter > 0)break;
             }else{
-               count++;
+               countLetter++;
             }
         }
         
-        return count;
+        return countLetter;
     }
 }
