6edac6c9a82d92fe6ca8e9b35955a400e50a7cca
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-05-11 15:24:49 +0200

    Time: 1 ms (99.28%), Space: 39.7 MB (81.90%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -5,18 +5,24 @@ class Solution {
         
         int right = x / 2;
         int left = 1;
+        int ans = 0;
         
         while (left <= right) {
             int mid = left + (right - left)/2;
             
+            if((long)mid*mid == x)return mid;
             if ((long)mid*mid > x) {
                 right = mid - 1;
             } else {
+                ans = mid;
                 left = mid + 1;
             }
         }
-        return right;
+        return ans;
         
         
     }
 }
+
+    
+    
