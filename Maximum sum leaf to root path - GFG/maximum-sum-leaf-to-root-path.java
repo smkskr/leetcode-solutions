915ca97e4abc1e7c9fe83939fce705bc2bc63743
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-06-06 20:09:34 +0200

    Added solution - LeetHub

--- Maximum sum leaf to root path - GFG/maximum-sum-leaf-to-root-path.java
+++ Maximum sum leaf to root path - GFG/maximum-sum-leaf-to-root-path.java
@@ -116,6 +116,7 @@ class Solution
     {
         //code here
         int[] maxSum = new int[1];
+        maxSum[0] = Integer.MIN_VALUE;
         maxPathSumUtil(root, 0, maxSum);
         return maxSum[0];
     }
