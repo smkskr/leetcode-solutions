63048f396529a59746edcf6b7c25d17a5e3043dd
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-06-09 14:51:19 +0200

    Time: 2 ms (16.29%), Space: 51.6 MB (7.51%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -14,26 +14,35 @@
  * }
  */
 class Solution {
+    
+    //TreeNode resultTree, temp;
+    
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
         
-        
-        if(root1 == null || root2 == null){
-            
-            if(root1 == null && root2 == null)return null;
-            if(root1 == null){
-                return root2;
-            }
-            if(root2 == null)
-                return root1;
-        }
-        
-        root1.val = root1.val + root2.val;
-        
-        root1.left = mergeTrees(root1.left,root2.left);
-        root1.right = mergeTrees(root1.right, root2.right);
-        
-        return root1;
+        return mergeTreesUtil(root1, root2);
+        //return resultTree;
     }
-
     
+    public TreeNode mergeTreesUtil(TreeNode root1, TreeNode root2){
+        
+        if(root1 == null)return cloneSubTree(root2);
+        if(root2 == null)return cloneSubTree(root1);
+        
+        TreeNode resultTree = new TreeNode(root1.val + root2.val);
+        resultTree.left = mergeTreesUtil(root1.left, root2.left);
+        resultTree.right = mergeTreesUtil(root1.right, root2.right);
+        
+        return resultTree;
+    }
+    
+    public TreeNode cloneSubTree(TreeNode root){
+        
+        if(root == null)return null;
+            
+        TreeNode resultTree = new TreeNode(root.val);
+        resultTree.left = cloneSubTree(root.left);
+        resultTree.right = cloneSubTree(root.right);
+        
+        return resultTree;
+    }
 }
