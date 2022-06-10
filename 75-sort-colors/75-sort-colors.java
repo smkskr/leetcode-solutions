c2d84302a64119d5e64cfa8f1dfc5d864759ffe3
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-06-10 21:52:47 +0200

    Time: 0 ms (100.00%), Space: 41.9 MB (69.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,34 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-       
         
-        int index = 0;
-        int leftPointer = 0;
-        int rightPointer = nums.length - 1;
+       int leftPointer = 0;
+       int rightPointer = nums.length - 1;
+       int index = 0;
         
-        // the idea is to shift zero to left of 1
-        // and shift two to right of 1
-        while(index <= rightPointer){
-            
-            if(nums[index] == 0){
+       while(index <= rightPointer){
+           
+           if(nums[index] == 0){
                swap(nums, leftPointer, index);
-                leftPointer++;index++;
-            }
-            else if(nums[index] == 2){
-                swap(nums, rightPointer, index);
-                rightPointer--;
-            }else 
-            index++;
-            
-        }
+               leftPointer++;index++;
+           }else if(nums[index] == 2){
+               swap(nums, rightPointer, index);
+               rightPointer--;
+           }else{
+               index++;
+           }
+       }
+        
         
     }
     
-    public void swap(int[] nums, int pos1, int pos2){
-        
-         int temp = nums[pos1];
-         nums[pos1] = nums[pos2];
-         nums[pos2] = temp;
-        
+    public void swap(int[] nums, int index1, int index2){
+        int cache = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = cache;
     }
 }
