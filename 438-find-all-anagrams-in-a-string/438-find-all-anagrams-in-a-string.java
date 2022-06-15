19c631ad698e06bb597114d6ea6a5a40fa45f897
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-06-15 02:03:39 +0200

    Time: 11 ms (74.47%), Space: 47.9 MB (50.24%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,32 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-        int start = 0;
-        int end = 0;
-        int lengthS = s.length();
-        int lengthP = p.length();
-        
-        HashMap<Character, Integer> countMap = new HashMap<>();
-        int countP = 0;
+        int[] charCountMap = new int[26];
         List<Integer> list = new ArrayList<>();
-        
+
         for(char ch : p.toCharArray()){
-            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);    
+            charCountMap[ch - 'a']++;
         }
         
-        while(end < lengthS){
+        int start = 0;
+        int end = 0;
+        int lengthOfMainString = s.length();
+        int windowSize = p.length();
+        
+        int count = 0;
+        
+        while(end < lengthOfMainString){
             
             char ch = s.charAt(end);
-            countMap.put(ch, countMap.getOrDefault(ch, 0) - 1);
-            if(countMap.get(ch) >= 0)countP++;
+            charCountMap[ch - 'a']--;
+            if(charCountMap[ch - 'a'] >= 0)count++;
             
-            if(end - start + 1 == lengthP){
+
+            
+            if(end - start + 1 == windowSize){
                 
-                if(countP == lengthP)list.add(start);
-                char firstChar = s.charAt(start++);
-                countMap.put(firstChar, countMap.get(firstChar) + 1);
-                if(countMap.get(firstChar) >= 1)countP--;
+                if(count == windowSize)list.add(start);
+                char firstCharacter = s.charAt(start);
+                if(charCountMap[firstCharacter - 'a']++ >= 0){
+                    count--;
+                }
+                
+                start++;
             }
+            
             end++;
         }
         
