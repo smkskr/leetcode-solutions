9ee37e8c6c4b12d2d8b8b8936e8cff646e4e4730
Saumik Sarkar <saumik.sarkar30@gmail.com>
2022-06-17 13:09:21 +0200

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(S[0]);
+            int E = Integer.parseInt(S[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for(int i = 0; i < V; i++){
+                adj.add(new ArrayList<Integer>());
+            }
+            for(int i = 0; i < E; i++){
+                String[] s = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(V, adj);
+            if(ans)
+                System.out.println("1");
+            else System.out.println("0");
+       }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        // Code here
+        int[] color = new int[V];
+        Arrays.fill(color, -1);
+        
+        for(int vertex = 0;vertex < V;vertex++){
+            if(color[vertex] == -1){
+                if(!isBipartite(adj, vertex, color)){
+                   return false;
+                }   
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean isBipartite(ArrayList<ArrayList<Integer>> adj, int source, int[] color){
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        color[source] = 1;
+        
+        while(!queue.isEmpty()){
+            
+            int u = queue.poll();
+            for(int v : adj.get(u)){
+                
+                 if(color[v] == -1){
+                    color[v] = 1 - color[u];
+                    queue.add(v);
+                }
+                else if(color[u] == color[v])return false;
+            }
+        }
+        return true;
+    }
+}
